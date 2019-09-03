447f5e5670a0e07e28456de0368df659ab975dad
schwarro <rob.ap.schwartz@gmail.com>
2019-09-03 16:14:07 -0400

    working with components

--- jsx/index.js
+++ jsx/index.js
@@ -1,8 +1,27 @@
+// import React from "react"
+// import ReactDOM from "react-dom"
+//
+// // JSX
+// ReactDOM.render(<div><h1>Hello world!</h1><p>This is a paragraph</p></div>, document.getElementById("root"))
+//
+// var myNewP = document.createElement("p")
+// myNewP.innerHTML = "This is a paragraph."
+
 import React from "react"
 import ReactDOM from "react-dom"
 
-// JSX
-ReactDOM.render(<div><h1>Hello world!</h1><p>This is a paragraph</p></div>, document.getElementById("root"))
+function MyInfo() {
+  return (
+    <div>
+      <h1>Bob Ziroll</h1>
+      <p>This is a paragraph about me...</p>
+      <ul>
+        <li>Thailand</li>
+        <li>Japan</li>
+        <li>Nordic Countries</li>
+      </ul>
+    </div>
+  )
+}
 
-var myNewP = document.createElement("p")
-myNewP.innerHTML = "This is a paragraph."
+ReactDOM.render(<MyInfo />, document.getElementById("root"))
