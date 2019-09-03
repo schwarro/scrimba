19cb6ddf3e5cdfeb3a5e73895e1cc3331fe0c128
schwarro <rob.ap.schwartz@gmail.com>
2019-09-03 16:28:53 -0400

    working with adding multiple components

--- jsx/index.js
+++ jsx/index.js
@@ -7,21 +7,28 @@
 // var myNewP = document.createElement("p")
 // myNewP.innerHTML = "This is a paragraph."
 
+// import React from "react"
+// import ReactDOM from "react-dom"
+//
+// function MyInfo() {
+//   return (
+//     <div>
+//       <h1>Bob Ziroll</h1>
+//       <p>This is a paragraph about me...</p>
+//       <ul>
+//         <li>Thailand</li>
+//         <li>Japan</li>
+//         <li>Nordic Countries</li>
+//       </ul>
+//     </div>
+//   )
+// }
+//
+// ReactDOM.render(<MyInfo />, document.getElementById("root"))
+
 import React from "react"
 import ReactDOM from "react-dom"
 
-function MyInfo() {
-  return (
-    <div>
-      <h1>Bob Ziroll</h1>
-      <p>This is a paragraph about me...</p>
-      <ul>
-        <li>Thailand</li>
-        <li>Japan</li>
-        <li>Nordic Countries</li>
-      </ul>
-    </div>
-  )
-}
+import MyInfo from "./components/MyInfo"
 
 ReactDOM.render(<MyInfo />, document.getElementById("root"))
