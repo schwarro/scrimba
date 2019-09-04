4dbbedadfb0da15866cd718c6eddc706759e609a
schwarro <rob.ap.schwartz@gmail.com>
2019-09-04 13:04:56 -0400

    reformated todos and styled

--- todo1/src/App.js
+++ todo1/src/App.js
@@ -1,13 +1,16 @@
 import React from 'react';
 import logo from './logo.svg';
-// import './App.css';
+import './App.css';
 
-import Todos from './components/Todos';
+import TodoItem from './components/TodoItem';
 
 function App() {
   return (
-    <div className="App">
-      <Todos />
+    <div className="todo-list">
+      <TodoItem />
+      <TodoItem />
+      <TodoItem />
+      <TodoItem />
     </div>
   );
 }
