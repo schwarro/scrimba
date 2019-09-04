de02fd9fcc7fecd77f52f29eb70810df487508af
schwarro <rob.ap.schwartz@gmail.com>
2019-09-04 11:07:28 -0400

    init react todo app and added place holder text

---
+++ todo1/src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import logo from './logo.svg';
+// import './App.css';
+
+import Todos from './components/Todos';
+
+function App() {
+  return (
+    <div className="App">
+      <Todos />
+    </div>
+  );
+}
+
+export default App;
---
+++ todo1/src/components/Todos.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+function Todos() {
+  return (
+    <div>
+      <input type="checkbox" />
+      <p>Placeholder text</p>
+      
+      <input type="checkbox" />
+      <p>Placeholder text</p>
+      
+      <input type="checkbox" />
+      <p>Placeholder text</p>
+      
+    </div>
+  )
+}
+
+export default Todos
