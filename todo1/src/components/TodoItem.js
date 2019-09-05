501627c96ac0f0a112a6ea57d6431a942c49a013
schwarro <rob.ap.schwartz@gmail.com>
2019-09-05 13:04:15 -0400

    added onClick feature

--- todo1/src/components/TodoItem.js
+++ todo1/src/components/TodoItem.js
@@ -1,9 +1,14 @@
 import React from 'react';
 
 function TodoItem(props) {
+
   return (
     <div className="todo-item">
-      <input type="checkbox"checked={props.item.completed} />
+      <input
+        type="checkbox"
+        checked={props.item.completed}
+        onClick={() => console.log("Clicked")}
+      />
       <h3>Todo: {props.item.text}</h3>
       <hr/>
     </div>
