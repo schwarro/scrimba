2fab6e9647288dbf2e9efb7ea47a0b324db6ba3b
schwarro <rob.ap.schwartz@gmail.com>
2019-09-05 12:43:46 -0400

    worked with adding state

--- state/src/App.js
+++ state/src/App.js
@@ -1,20 +1,42 @@
 import React from "react"
 
 class App extends React.Component {
+    // constructor() {
+    //   super()
+    //   this.state = {
+    //     name: "Rob",
+    //     age: 31
+    //   }
+    // }
+    //
+    // render() {
+    //   return (
+    //       <div>
+    //           <h1>{this.state.name}</h1>
+    //           <h3>{this.state.age} years old</h3>
+    //       </div>
+    //   )
+    // }
+
     constructor() {
       super()
       this.state = {
-        name: "Rob",
-        age: 31
+        isLoggedIn: true
       }
     }
 
     render() {
+      let x
+
+      if(this.state.isLoggedIn === true) {
+        x = "in"
+      } else {
+        x = "out"
+      }
       return (
-          <div>
-              <h1>{this.state.name}</h1>
-              <h3>{this.state.age} years old</h3>
-          </div>
+        <div>
+          <h1>You are currently logged {x}</h1>
+        </div>
       )
     }
 }
