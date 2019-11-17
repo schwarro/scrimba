06f8b0382b6697227627cdae38741e01f4745b7a
schwarro <rob.ap.schwartz@gmail.com>
2019-11-17 10:00:35 -0500

    working with the dom

--- test-app/src/App.js
+++ test-app/src/App.js
@@ -4,7 +4,11 @@ import React from 'react';
 function App() {
   return (
     <div className="App">
-
+      <ul>
+        <li>Something 1</li>
+        <li>Something 2</li>
+        <li>Something 3</li>
+      </ul>
     </div>
   );
 }
