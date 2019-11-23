23a3297d85758bff9c10bd3264d5d4b9cb682b33
schwarro <rob.ap.schwartz@gmail.com>
2019-11-23 16:25:41 -0500

    working with multiple components

---
+++ test-app/src/App.js
@@ -0,0 +1,14 @@
+import React from "react"
+import MyInfo from './components/MyInfo.js';
+import Footer from './components/Footer.js'
+
+function App() {
+  return (
+    <div>
+      <MyInfo />
+      <Footer />
+    </div>
+  )
+}
+
+export default App
