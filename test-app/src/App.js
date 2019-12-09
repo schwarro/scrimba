fc788c65ce45d48c4f4d5efeee6333eab847d5ae
schwarro <rob.ap.schwartz@gmail.com>
2019-12-09 13:02:11 -0500

    added multiple components to app.js

--- test-app/src/App.js
+++ test-app/src/App.js
@@ -1,10 +1,15 @@
 import React from "react"
 import MainContent from "./components/MainContent.js"
+import Header from "./components/Header.js"
+import Footer from "./components/Footer.js"
+
 
 function App() {
   return (
     <div>
+      <Header />
       <MainContent />
+      <Footer />
     </div>
   )
 }
---
+++ test-app/src/components/Footer.js
@@ -0,0 +1,11 @@
+import React from "react"
+
+function Footer() {
+  return (
+    <header>
+      This is the footer.
+    </header>
+  )
+}
+
+export default Footer
