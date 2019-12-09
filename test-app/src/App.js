9e77f8e939df4e71c60aff0a9f2b9278ee0adfd2
schwarro <rob.ap.schwartz@gmail.com>
2019-12-09 13:32:37 -0500

    working with jsx

--- test-app/src/App.js
+++ test-app/src/App.js
@@ -5,11 +5,28 @@ import Footer from "./components/Footer.js"
 
 
 function App() {
+  const firstName = "Rob"
+  const lastName = "Schwartz"
+
+  const date = new Date()
+  const hours = date.getHours()
+  let timeOfDay
+
+  if (hours < 12) {
+    timeOfDay = "morning"
+  } else if (hours >= 12 && hours < 17) {
+    timeOfDay = "afternoon"
+  } else {
+    timeOfDay = "night"
+  }
+
   return (
     <div>
-      <Header />
+      {/* <Header />
       <MainContent />
-      <Footer />
+      <Footer /> */}
+      <h1>Good {timeOfDay} {firstName}!</h1>
+      <h2>It is currently about {hours % 12} o'clock!</h2>
     </div>
   )
 }
