9157d8a49ebd8c174f2719810c9b88ea9238913a
schwarro <rob.ap.schwartz@gmail.com>
2019-12-10 16:14:01 -0500

    working with react props

--- test-app/src/App.js
+++ test-app/src/App.js
@@ -2,6 +2,7 @@ import React from "react"
 import MainContent from "./components/MainContent.js"
 import Header from "./components/Header.js"
 import Footer from "./components/Footer.js"
+import ContactCard from "./components/ContactCard.js"
 
 
 function App() {
@@ -30,8 +31,43 @@ function App() {
   // }
 
   return (
-    <div>
-      <MainContent />
+    <div className="contacts">
+      <ContactCard
+          contact={{
+            name: "Mr. Whiskerson",
+            imgUrl: "http://placekitten.com/300/200",
+            phone: "(212) 555-1234",
+            email: "mr.whiskaz@catnap.meow"
+          }}
+      />
+
+      <ContactCard
+          contact={{
+            name:"Fluffykins",
+            imgUrl:"http://placekitten.com/400/200",
+            phone:"(212) 555-2345",
+            email:"fluff@me.com"
+          }}
+      />
+
+      <ContactCard
+          contact={{
+            name:"Destroyer",
+            imgUrl:"http://placekitten.com/400/300",
+            phone:"(212) 555-3456",
+            email:"ofworlds@yahoo.com"
+          }}
+      />
+
+      <ContactCard
+          contact={{
+            name:"Felix",
+            imgUrl:"http://placekitten.com/200/100",
+            phone:"(212) 555-4567",
+            email:"thecat@hotmail.com"
+          }}
+      />
+      {/* <MainContent /> */}
       {/* <Header />
       <Footer /> */}
       {/* <h1 style={styles}>Good {timeOfDay} {firstName}!</h1>
