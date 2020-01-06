717f91b2a35627ab9b657faf0d34c2a0be351706
schwarro <rob.ap.schwartz@gmail.com>
2020-01-06 13:51:28 -0500

    working with stte

--- practice/src/App.js
+++ practice/src/App.js
@@ -1,26 +1,33 @@
 import React, {Component} from "react"
 
-// Challenge:
-// Given an incomplete class-based component without a constructor,
-// add a constructor and initialize state to fix the broken component.
+// Given a stateless functional component, add state to it
+// state should have a property called `isLoggedIn` which is a boolean
+// (true if logged in, false if not)
+// Then, give your best shot at rendering the word "in" if the user is logged in
+// or "out" if the user is logged out.
 
 class App extends Component {
     constructor() {
         super()
         this.state = {
-            name: "Rob",
-            age: 31
+            loggedIn: false
         }
     }
+
     render() {
+          let x
+
+          if(this.state.loggedIn){
+              x = "in";
+          } else {
+              x = "out";
+          }
         return (
             <div>
-                <h1>{this.state.name}</h1>
-                <h3>{this.state.age} years old</h3>
+                <h1>You are currently logged {x}</h1>
             </div>
         )
     }
-
 }
 
 export default App
