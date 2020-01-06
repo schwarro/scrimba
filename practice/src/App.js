7db3b0444bae64698b93017b88d75ede5fe57749
schwarro <rob.ap.schwartz@gmail.com>
2020-01-06 13:38:19 -0500

    working with state

--- practice/src/App.js
+++ practice/src/App.js
@@ -1,26 +1,26 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {Component} from "react"
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+// Challenge:
+// Given an incomplete class-based component without a constructor,
+// add a constructor and initialize state to fix the broken component.
+
+class App extends Component {
+    constructor() {
+        super()
+        this.state = {
+            name: "Rob",
+            age: 31
+        }
+    }
+    render() {
+        return (
+            <div>
+                <h1>{this.state.name}</h1>
+                <h3>{this.state.age} years old</h3>
+            </div>
+        )
+    }
+
 }
 
-export default App;
+export default App
