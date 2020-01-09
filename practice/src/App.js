ccd7965bf95195c6836e1073bc68e75f18a99113
schwarro <rob.ap.schwartz@gmail.com>
2020-01-09 17:30:44 -0500

    react conditional render practice

--- practice/src/App.js
+++ practice/src/App.js
@@ -1,35 +1,49 @@
 import React, {Component} from "react"
-import Conditional from "./Conditional"
-import Header from "./Header"
-import Footer from "./Footer"
+/*
+Given a stateless functional component:
+1. Follow the steps necessary to add state to it,
+2. Have state keep track of whether the user is logged in or not
+3. Add a button that logs the user in/out
+    a. extra challenge - make the button display "log in" if they're not logged in and "log out" if they are
+4. Display text that says "Logged in" if the user is logged in, or "Logged out" if they're not.
+*/
 
 class App extends Component {
     constructor() {
-        super()
-        this.state = {
-            isLoading: true
-        }
+      super()
+      this.state = {
+        isLoggedIn: false
+      }
+      this.handleClick = this.handleClick.bind(this)
     }
 
-    componentDidMount() {
-        setTimeout(() => {
-            this.setState({
-                isLoading: false
-            })
-        }, 1500)
+    handleClick() {
+      this.setState(prevState => {
+        return {
+          isLoggedIn : !prevState.isLoggedIn
+        }
+      })
     }
 
     render() {
-        return (
-            <div>
-                <Header />
-                {this.state.isLoading ?
-                <h1>Loading...</h1> :
-                <Conditional />}
-                <Footer />
-            </div>
-        )
+
+      let wordDisplay
+      let buttonDisplay
+
+      this.state.isLoggedIn ? wordDisplay = "in" : wordDisplay = "out"
+
+      this.state.isLoggedIn ? buttonDisplay = "out" : buttonDisplay = "in"
+
+      return (
+          <div>
+              <h1>You are currently logged {wordDisplay}</h1>
+              <button onClick={this.handleClick}>
+                Log {buttonDisplay}
+              </button>
+          </div>
+      )
     }
+
 }
 
 export default App
